869a2838f3e63323c61dfe3f184cbe408addd264
Kayv1e <krutojpacanivan@gmail.com>
2025-11-01 12:40:28 +0300

    Edited main function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
 
+int **make(int r, int c);
+void output(const int* const* mtx);
+void rm(int** mtx, int r);
+
 int main()
-{}
+{
+  int rows = 0;
+  int cols = 0;
+  std::cin >> rows >> cols;
+  if (std::cin.fail()) {
+    return 1;
+  }
+  int** mtx = nullptr;
+  mtx = make(rows, cols);
+  output(mtx);
+  rm(mtx, rows);
+}
